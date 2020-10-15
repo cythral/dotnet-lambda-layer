88f378c39a483798a0e83640b060434bdea1825b
Talen Fisher <talen.fisher@cythral.com>
2020-10-14 20:05:28 -0500

    Remove Asp.Net Core Stuff

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 mkdir bin
 dotnet-install.sh --jsonfile global.json --install-dir bin
-rm -rf bin/templates bin/sdk
+rm -rf bin/templates bin/sdk bin/shared/Microsoft.AspNetCore.App bin/packs/Microsoft.AspNetCore.App.Ref
+rm -rf 
 
 cp bootstrap bin
 chmod +x bin/bootstrap
