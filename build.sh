e07f22ef132ee688d5042effb750df72b6d7775f
Talen Fisher <talen.fisher@cythral.com>
2020-11-10 13:23:36 -0600

    Add MIT License

--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@
 mkdir bin
 dotnet-install.sh --jsonfile global.json --install-dir bin
 rm -rf bin/templates bin/sdk bin/shared/Microsoft.AspNetCore.App bin/packs/Microsoft.AspNetCore.App.Ref
-rm -rf 
 
-cp bootstrap bin
+cp LICENSE.txt bootstrap bin
 chmod +x bin/bootstrap
 
